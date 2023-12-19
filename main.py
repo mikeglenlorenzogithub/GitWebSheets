04acb9cbd50cb664b32233686361301a27137b2b
Michael Glen Lorenzo <mikeglenlorenzo.github@gmail.com>
2023-12-19 09:28:38 +0700

    revision main.py del dup code

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from flask import Flask
 from website import create_app, read_sheets
 
 app = create_app()
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'sc_bc_ac.json'
-
-app = create_app()
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '.json'
 
 if __name__ == '__main__':
     app.run(debug=True)

575564828b42d0d1717d938526eca50db135a9ba
Michael Glen Lorenzo <mikeglenlorenzo.github@gmail.com>
2023-12-19 09:29:51 +0700

    revision run_scraping.py execute with file_name

--- website/static/run_scraping.py
+++ website/static/run_scraping.py
@@ -1,6 +1,7 @@
 from website.static.programs import get_program, run_program
 
-def run_script():
+def run_script(file_name):
     list_program = get_program()
     for program in list_program:
-        execute_program = run_program(program)
+        if file_name in program:
+            execute_program = run_program(file_name)

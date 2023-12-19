a4485f84bf37a8f323da0e02f5dbdbe5fbf1de07
Michael Glen Lorenzo <mikeglenlorenzo.github@gmail.com>
2023-12-19 09:31:37 +0700

    revision views.py single run and add flash

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect
+import time
+import werkzeug.exceptions as ex
 from website.static.run_scraping import run_script
 
 views = Blueprint('views', __name__)
@@ -11,7 +13,54 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", route='home_page')
 
-@views.route('/scraping', methods=['POST', 'GET'])
-def scraping():
-    run_script()
+# RUN PYTHON SCRIPT IN HTML
+@views.route('/scraping-abahouse', methods=['POST', 'GET'])
+def scrapingAbahouse():
+    if request.method == 'POST':
+        try:
+            run_script('shopping_apparel_abahouse')
+            flash('The scraping process is done', category='success')
+        except:
+            flash('There is some trouble while scraping the website', category='error')
+            raise
+    return render_template("home.html", route='home_page')
+
+@views.route('/scraping-adabat', methods=['POST', 'GET'])
+def scrapingAdabat():
+    if request.method == 'POST':
+        try:
+            run_script('shopping_apparel_adabat')
+            flash('The scraping process is done', category='success')
+        except:
+            flash('There is some trouble while scraping the website', category='error')
+    return render_template("home.html", route='home_page')
+
+@views.route('/scraping-adametrope', methods=['POST', 'GET'])
+def scrapingAdametrope():
+    if request.method == 'POST':
+        try:
+            run_script('shopping_apparel_adametrope')
+            flash('The scraping process is done', category='success')
+        except:
+            flash('There is some trouble while scraping the website', category='error')
+    return render_template("home.html", route='home_page')
+
+@views.route('/scraping-anteprima', methods=['POST', 'GET'])
+def scrapingAnteprima():
+    if request.method == 'POST':
+        try:
+            run_script('shopping_apparel_anteprima')
+            flash('The scraping process is done', category='success')
+        except:
+            flash('There is some trouble while scraping the website', category='error')
+    return render_template("home.html", route='home_page')
+
+@views.route('/scraping-bape', methods=['POST', 'GET'])
+def scrapingBape():
+    if request.method == 'POST':
+        try:
+            run_script('shopping_apparel_bape')
+            flash('The scraping process is done', category='success')
+        except:
+            flash('There is some trouble while scraping the website', category='error')
     return render_template("home.html", route='home_page')
